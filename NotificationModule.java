52a941dbaca2faec554b1172e2ea209e00009865
Nada511 <67808560+Nada511@users.noreply.github.com>
2020-11-27 21:32:10 +0200

    Add files via upload

--- NotificationModule.java
+++ NotificationModule.java
@@ -1,103 +1,150 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class NotificationModule {
-	
-	ArrayList <Notification>  notifications;
-	ArrayList <NotificationTemplate> templates;
-	ArrayList <NotificationTemplate> filledTemplates;
-	private Scanner scanner;
-	
-	public void storeTemplates(String path) throws IOException {
-		File folder = new File(path);
-		if(folder.isDirectory()) {
-			for (int i=0; i<templates.size(); i++ ){                    // iterate over templates
-				if (!folder.exists()){
-					String fileNum = "file"+Integer.toString(i); 
-					File templateFile = new File(fileNum);                 // create file by number
-					scanner = new Scanner(templateFile);   
-					if (scanner.hasNextLine()) {
-						BufferedWriter writer = new BufferedWriter(new FileWriter(fileNum));
-						//templates.get(i).setSubject(scanner.nextLine());
-						writer.write(templates.get(i).getSubject());
-						writer.close();
-							}
-					while (scanner.hasNextLine()) {
-						BufferedWriter writer = new BufferedWriter(new FileWriter(fileNum));
-						writer.write(templates.get(i).getContent());
-						writer.close();
-						}
-					}														
-					                                                      
-				}
-			}	
-		}
-	public void readTemplates(String path) throws FileNotFoundException {
-				File template = new File(path);   
-				scanner = new Scanner(template);   // iterate over files                                        
-				//ArrayList <NotificationTemplate> storedTemplates = new ArrayList<>();          // create new template object
-				if (scanner.hasNextLine()) {
-					System.out.println(scanner.nextLine());
-				}
-				while (scanner.hasNextLine()){ 
-					System.out.println(scanner.nextLine());
-				}
-				scanner.close();															                                                     
-			}
-					         
-	public void createTemplate(String subject, String content, String placeholder)  {
-		
-		TemplateParser parser = new TemplateParser();
-		ArrayList<Integer> parsedContent = parser.parse(content, placeholder);
-		parsedContent.add(1);
-		NotificationTemplate newTemplate = new NotificationTemplate();
-		
-		newTemplate.setContent(content);
-		newTemplate.setPlaceholder(parsedContent);
-		newTemplate.setSubject(subject);
-		
-		templates.add(newTemplate);
-		
-	}
-	
-	public void updateTemplate(String subject, String newContent, String newPlaceholder) {
-		// search for template by subject
-		// get old template file index
-		// delete old template file
-		// update content in arraylist
-		// update placeholder in arraylist
-		// save new template file
-	}
-	
-	public void deleteTemplate(String subject) {
-		// search for template by subject
-		// delete from arraylist
-		// delete template file
-	}
-	
-	public void fillTemplate(String subject, ArrayList<String> values) {  
-		NotificationTemplate newTemplate = new NotificationTemplate();           // search for template by subject in templates
-		for (int i=0; i<templates.size(); i++) {
-			if (templates.get(i).getSubject().equalsIgnoreCase(subject)) {
-				newTemplate.setSubject(templates.get(i).getSubject());              // create copy of template (newTemplate)
-				newTemplate.setPlaceholder(templates.get(i).getPlaceholder());
-				newTemplate.setContent(templates.get(i).getContent());
-				newTemplate.insertValues(values);                                 
-			}                  
-			filledTemplates.add(newTemplate);                                       // add newTemplate to filledTemplates
-		}
-	}
-	public NotificationTemplate findTemplate(String subject) {
-		return null;
-		// search for template by subject in arraylist
-		// return null if not found
-	}
-	
-	
+
+    ArrayList<NotificationTemplate> templates;
+    ArrayList<NotificationTemplate> filledTemplates;
+
+    NotificationModule() {
+        templates = new ArrayList<>();
+        filledTemplates = new ArrayList<>();
+    }
+
+    public boolean createTemplate(String subject, String content, String placeholder) {
+
+        TemplateParser parser = new TemplateParser();
+        ArrayList<Integer> parsedContent = parser.parse(content, placeholder);
+
+        if (!parsedContent.equals(null)) {
+            NotificationTemplate newTemplate = new NotificationTemplate();
+            newTemplate.setContent(content);
+            newTemplate.setPlaceholderIndices(parsedContent);
+            newTemplate.setSubject(subject);
+            templates.add(newTemplate);
+            return true;
+        } else return false;
+
+    }
+
+    public boolean readTemplate(Source source) {
+        String data = source.readSource();
+        if (!source.readSource().equals(null)) {
+            String placeholder = "";
+            String subject = "";
+            String content = "";
+            int i = 0;
+            // fetch placeholder
+            while (i < data.length()) {
+                if (data.charAt(i) == '\n') break;
+                placeholder += data.charAt(i);
+                i++;
+            }
+            // fetch subject
+            while (i < data.length()) {
+                if (data.charAt(i) == '\n') break;
+                subject += data.charAt(i);
+                i++;
+            }
+            // fetch content
+            while (i < data.length()) {
+                content += data.charAt(i);
+                i++;
+            }
+            NotificationTemplate newTemplate = new NotificationTemplate();
+            TemplateParser parser = new TemplateParser();
+            ArrayList<Integer> parsedContent = parser.parse(content, placeholder);
+            newTemplate.setPlaceholder(placeholder);
+            newTemplate.setSubject(subject);
+            newTemplate.setContent(content);
+            newTemplate.setPlaceholderIndices(parsedContent);
+            templates.add(newTemplate);
+            return true;
+        } else return false;
+    }
+
+    public boolean updateTemplate(Source source, String newContent, String newPlaceholder) {
+        String data = source.readSource();
+        boolean flag = false;
+        if (!data.equals(null)) {
+            int i = data.indexOf('\n') + 1;
+            if (i >= 0) {
+                String subject = "";
+                for (; i < data.length(); i++) {
+                    if (data.charAt(i) == '\n') break;
+                    subject += data.charAt(i);
+                }
+                NotificationTemplate matchingTemplate = findTemplate(subject);
+                matchingTemplate.setContent(newContent);
+                matchingTemplate.setPlaceholder(newPlaceholder);
+                data = newPlaceholder + '\n';
+                data += subject + '\n';
+                data += newContent;
+                flag = source.writeToSource(data);
+            }
+        }
+        return flag;
+    }
+
+    public boolean deleteTemplate(Source source) {
+        boolean flag = false;
+        String data = source.readSource();
+        if (!data.equals(null)) {
+            int i = data.indexOf('\n') + 1;
+            if (i >= 0) {
+                String subject = "";
+                for (; i < data.length(); i++) {
+                    if (data.charAt(i) == '\n') break;
+                    subject += data.charAt(i);
+                }
+                NotificationTemplate matchingTemplate = findTemplate(subject);
+                templates.remove(matchingTemplate);
+                flag = source.deleteSource();
+            }
+        }
+        return flag;
+    }
+
+    public boolean storeTemplate(Source source, String subject) {
+        NotificationTemplate matchingTemplate = findTemplate(subject);
+        boolean flag = false;
+        if (!matchingTemplate.equals(null)) {
+            String data = "";
+            data += matchingTemplate.getPlaceholder() + "\n";
+            data += matchingTemplate.getSubject() + "\n";
+            data += matchingTemplate.getContent();
+            flag = source.writeToSource(data);
+        }
+        return flag;
+    }
+
+    public void fillTemplate(String subject, ArrayList<String> values) {
+        NotificationTemplate matchingTemplate = findTemplate(subject);
+        if (!matchingTemplate.equals(null)) {
+            NotificationTemplate newTemplate = new NotificationTemplate();
+            newTemplate.setSubject(matchingTemplate.getSubject());
+            newTemplate.setPlaceholder(matchingTemplate.getPlaceholder());
+            newTemplate.setContent(matchingTemplate.getContent());
+            newTemplate.setPlaceholderIndices(matchingTemplate.getPlaceholderIndices());
+            newTemplate.insertValues(values);
+            filledTemplates.add(newTemplate);
+        }
+    }
+
+    public NotificationTemplate findTemplate(String subject) {
+        int i = 0;
+        boolean found = false;
+        while (i < templates.size()) {
+            if (subject.equals(templates.get(i).getSubject())) {
+                found = true;
+                break;
+            }
+        }
+        if (found)
+            return templates.get(i);
+        else
+            return null;
+    }
 }
--- NotificationTemplate.java
+++ NotificationTemplate.java
@@ -1,45 +1,68 @@
 import java.util.ArrayList;
 
 public class NotificationTemplate {
-	
-	String subject;
-	String content;
-	String placeholder;
-	ArrayList<Integer> placeholderIndices;
 
-	public void setSubject(String subject) {
-		this.subject=subject;
-	}
-	public String getSubject() {
-		return subject;
-	}
-	
-	public void setContent(String content) {
-		this.content=content;
-	}
-	public String getContent () {
-		return content;
-	}
-	
-	public void setPlaceholder(String placeholder) {
-		this.placeholder=placeholder;
-	}
-	public String getPlaceholder () {
-		return placeholder;
-	}
-	
-	public void setPlaceholderIndices(ArrayList<Integer> placeholderIndices) {
-		this.placeholderIndices=placeholderIndices;
-	}
-	public ArrayList<Integer> getPlaceholderIndices() {
-		return placeholderIndices;
-	}
-	
-	public void insertValues(ArrayList<String> values) {		
-		// if values.size() != placeholderIndices.size()
-			// equalize with empty strings
-		// for loop, starting 0, ending at placeholderIndices.size()
-		// if value is not empty string
-			// replace placeholder with value at same index
-	}
+    String subject;
+    String content;
+    String placeholder;
+    ArrayList<Integer> placeholderIndices;
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setPlaceholder(String placeholder) {
+        this.placeholder = placeholder;
+    }
+
+    public String getPlaceholder() {
+        return placeholder;
+    }
+
+    public void setPlaceholderIndices(ArrayList<Integer> placeholderIndices) {
+        this.placeholderIndices = placeholderIndices;
+    }
+
+    public ArrayList<Integer> getPlaceholderIndices() {
+        return placeholderIndices;
+    }
+
+    public void insertValues(ArrayList<String> values) {
+        if (values.size() != placeholderIndices.size()) {
+            if (values.size() < placeholderIndices.size()) {
+                for (int i = values.size(); i < placeholderIndices.size(); i++)
+                    values.add(null);
+            } else {
+                for (int i = placeholderIndices.size(); i < values.size(); i++)
+                    values.remove(i);
+            }
+        }
+        int k = 0;
+        for (int i = 0; i < content.length(); i++) {
+            if (content.charAt(i) == placeholder.charAt(0)) {
+                String remove = "";
+                for (int j = 0; j < placeholder.length(); j++) {
+                    remove += content.charAt(i + j);
+                }
+                if (remove.equals(placeholder)) {
+                    String part1 = content.substring(0, i);
+                    String part2 = content.substring(i + placeholder.length());
+                    content = part1 + values.get(k) + part2;
+                    k++;
+                }
+            }
+        }
+    }
 }
