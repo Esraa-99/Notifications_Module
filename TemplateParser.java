bd116afdd0be561b273e717474e9692a4f4276bd
mohannadaly <mohannadaly98@gmail.com>
2020-11-27 11:00:55 +0200

    Add preliminary parse implementation

--- TemplateParser.java
+++ TemplateParser.java
@@ -5,7 +5,18 @@ public class TemplateParser {
 	private ArrayList <Integer> placeholderIndex;
 	
 	public ArrayList<Integer> parse(String content, String placeholder){
-		
+		String[] splitContent = content.split(placeholder);
+
+		int placeholderLength = placeholder.length();
+		int index = 0;
+
+		for (String item : splitContent) {
+			index += item.length();
+			placeholderIndex.add(index);
+			if (index + placeholderLength < content.length())
+				index += placeholderLength;
+		}
+
 		return placeholderIndex;
 	}
 	
