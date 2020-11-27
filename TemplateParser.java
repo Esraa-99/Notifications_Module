d99759fcfdee5d8672793dfa80094e1d86cfc9fb
mohannadaly <mohannadaly98@gmail.com>
2020-11-27 10:56:51 +0200

    Add Main Class

--- TemplateParser.java
+++ TemplateParser.java
@@ -1,12 +1,23 @@
 import java.util.ArrayList;
 
 public class TemplateParser {
-	
-	private ArrayList <Integer> placeholderIndex;
-	
-	public ArrayList<Integer> parse(String content, String placeholder){
-		
-		return placeholderIndex;
-	}
-	
+
+    private ArrayList<Integer> placeholderIndex;
+
+    public ArrayList<Integer> parse(String content, String placeholder) {
+        String[] splitContent = content.split(placeholder);
+
+        int placeholderLength = placeholder.length();
+        int index = 0;
+
+        for (String item : splitContent) {
+            index += item.length();
+            placeholderIndex.add(index);
+            if (index + placeholderLength < content.length())
+                index += placeholderLength;
+        }
+
+        return placeholderIndex;
+    }
+
 }
