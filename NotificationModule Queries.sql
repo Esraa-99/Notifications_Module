59dd1e36dd0fbb48faf56a5c092cd3ed7d850765
mohannadaly <mohannadaly98@gmail.com>
2020-12-27 21:57:13 +0200

    Upload SQL

---
+++ NotificationModule Queries.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE NotificationModule;
+
+CREATE TABLE Templates
+(
+    placeholder varchar(10) NOT NULL,
+    subject varchar(100) NOT NULL,
+    content TEXT NOT NULL,
+    PRIMARY KEY (subject)
+);
+
+INSERT INTO Templates (placeholder, subject, content) VALUES ('{x}', 'Test Template', 'Hello, my name is {x}');
+
+CREATE TABLE Notifications(
+	notificationID  int NOT NULL AUTO_INCREMENT,
+	subject varchar(100) NOT NULL,
+    content TEXT NOT NULL,
+    channel varchar(10) NOT NULL,
+    date date,
+    destination varchar(100) NOT NULL,
+    PRIMARY KEY (notificationID)
+);
