8579eb7880a1a7cab1e2d377c5a7bf44faeadd6e
mohannadaly <mohannadaly98@gmail.com>
2020-12-27 23:05:12 +0200

    Add null check

--- src/main/java/Notification_Module/Service/Notification.java
+++ src/main/java/Notification_Module/Service/Notification.java
@@ -26,17 +26,18 @@ public class Notification {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void send(NotificationController notification, @PathParam("channel") String channel) {
 		NotificationTemplate template = source.readSource(notification.getSubject());
-		template.insertValues(notification.getValues());
-		QueueHandler queue = new DBQueueHandler();
-		Channel notificationChannel = null;
-		if(channel.equalsIgnoreCase("email")) {
-			notificationChannel = (Email) new Email();
-			((Email) notificationChannel).setEmail_address(notification.getDestination());
+		if (template != null) {
+			template.insertValues(notification.getValues());
+			QueueHandler queue = new DBQueueHandler();
+			Channel notificationChannel = null;
+			if (channel.equalsIgnoreCase("email")) {
+				notificationChannel = (Email) new Email();
+				((Email) notificationChannel).setEmail_address(notification.getDestination());
+			} else if (channel.equalsIgnoreCase("sms")) {
+				notificationChannel = (SMS) new SMS();
+				((SMS) notificationChannel).setPhone_no(notification.getDestination());
+			}
+			queue.queing(template, notificationChannel, source);
 		}
-		else if(channel.equalsIgnoreCase("sms")) {
-			notificationChannel = (SMS) new SMS();
-			((SMS) notificationChannel).setPhone_no(notification.getDestination());
-		}
-		queue.queing(template, notificationChannel, source);
 	}
 }
