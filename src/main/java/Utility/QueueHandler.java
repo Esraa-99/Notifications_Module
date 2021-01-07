a9503248fffd624cad330dadbd5e46d6d060180d
Tuka-Ibrahim <66652903+Tuka-Ibrahim@users.noreply.github.com>
2021-01-08 01:07:20 +0200

    Update QueueHandler.java

--- src/main/java/Utility/QueueHandler.java
+++ src/main/java/Utility/QueueHandler.java
@@ -5,4 +5,6 @@ import Controllers.NotificationTemplate;
 public interface QueueHandler {
 	
 	public void queing(NotificationTemplate template, Channel channel, Source databaseCon);
+	public ArrayList <NotificationTemplate> dequeing(Channel channel, Source databaseCon);
+
 }
